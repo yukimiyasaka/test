c34f6127cbc9103ef75dc7311644f3bf528034d3
yukimiyasaka <36834327+yukimiyasaka@users.noreply.github.com>
2018-02-26 14:43:38 +0900

    Update bot.js

--- libs/bot.js
+++ libs/bot.js
@@ -79,7 +79,7 @@ function onText(line, event) {
       text: 'https://help.salesforce.com/articleView?id=user_password.htm&language=ja&type=0'
     }]
   },{
-    key: /.*playstation.*/,
+    key: /.*プレステ.*/,
     messages: [{
       type: 'text',
       text: 'こちらの情報はお役にたちますか？'
